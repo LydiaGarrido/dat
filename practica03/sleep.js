3c5fc3fc6f7604e104b7e99286206661c8552c15
Lydia Garrido Muñoz <l.garridomu@alumnos.urjc.es>
2019-03-18 13:34:06 +0100

    Realizada Práctica 3.5

--- practica03/sleep.js
+++ practica03/sleep.js
@@ -2,3 +2,18 @@
 
 /* Lydia Garrido Muñoz
 login: lgarrido */
+
+function sleep(milliseconds) {
+  let finish = Date.now() + milliseconds;
+  while (Date.now() - finish){
+    ;
+  }
+}
+
+function main() {
+  console.log('Se va a hacer un sleep de 4 segundos');
+  sleep(4000);
+  console.log('ya han pasado los 4 segundos');
+}
+
+main();
