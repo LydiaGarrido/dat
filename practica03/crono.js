5d535b781396cff4c9db280c1379467586f49daa
Lydia Garrido Muñoz <l.garridomu@alumnos.urjc.es>
2019-04-04 19:19:13 +0200

    Realizada práctica 4.1 y fallando 4.2

--- practica03/crono.js
+++ practica03/crono.js
@@ -3,4 +3,57 @@
 /* Lydia Garrido Muñoz
 login: lgarrido */
 
-function acepta_tecla(x, crono)
+function sleep(x) {
+  if (Math.sign(x) == -1){
+    return console.log("Error: los segundos no pueden ser negativos");
+  }else if (typeof x != 'number'){
+    return console.log("Error: el párametro no es válido");
+  }
+  let finish = Date.now() + (x * 1000);
+  while (Date.now() - finish){
+    ;
+  }
+}
+
+function check_pulse(x){
+  if(x == 's' or x == 'p' or x == 'r'){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+function acepta_tecla(x, crono){
+  if(check_pulse){
+    
+  }
+}
+
+function main(){
+
+console.log('Tecla incorrecta');
+console.log(acepta_tecla('x', crono));
+
+console.log('Uso normal');
+console.log(acepta_tecla('s', crono));
+sleep(1.5);
+console.log(acepta_tecla('s', crono));
+console.log(acepta_tecla('r', crono));
+
+
+console.log('Reset después de reset');
+sleep(.5);
+console.log(acepta_tecla('r', crono));
+
+
+sleep(.5);
+console.log('Uso normal con parciales');
+console.log(acepta_tecla('s', crono));
+sleep(1);
+console.log(acepta_tecla('p', crono));
+sleep(1.5);
+console.log(acepta_tecla('p', crono));
+sleep(1.5);
+console.log(acepta_tecla('s', crono));
+
+}
---
+++ practica04/js/js02.js
@@ -0,0 +1,37 @@
+#!/usr/bin/nodejs
+
+/* Lydia Garrido Muñoz
+login: lgarrido */
+
+'use strict'
+
+function kilometros(metros){
+	return metros * 3.6;
+}
+
+function metroshora(metros){
+	return (metros * (3600/ 1609.344));
+}
+
+function convert(metros, medida){
+	if(arguments.length != 2){
+		return("Error: número de argumentos incorrecto");
+	}
+	switch(medida){
+		case('m/s'):
+			return metros.toString();
+		case('km/h'):
+			return kilometros(metros).toString();
+		case('mph'):
+			return metroshora(metros).toString();
+		default:
+			return("Error: El segundo argumento es incorrecto");
+	}
+}
+
+var velocidad = 30;
+console.log(convert(velocidad, 'mph'));
+console.log(convert(velocidad, 'm/s'));
+console.log(convert(velocidad, 'km/h'));
+console.log(convert(velocidad));
+console.log(convert(velocidad, 'millas'));
