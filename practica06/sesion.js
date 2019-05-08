bfabf336bfc6b68f0c51d0907940788b9490a572
LydiaGarrido <l.garridomu@alumnos.urjc.es>
2019-05-08 18:25:46 +0200

    Empezada Práctica 6.2

--- practica06/sesion.js
+++ practica06/sesion.js
@@ -1,2 +1,29 @@
 /* Lydia Garrido Muñoz
 login: lgarrido */
+
+'use strict'
+
+let primera;
+let usuario = new Object();
+let nombreUsuario = localStorage.getItem('nombreUsuario');
+if (!nombreUsuario) {
+	let input = prompt("¿Cómo te llamas?");
+	localStorage.setItem('nombreUsuario', input);
+	primera = true;
+	usuario.fecha = new Date();
+	usuario.localizacion = getCurrentPosition();
+} else {
+	alert("Hola " + nombreUsuario);
+	primera = false;
+	usuario.fecha = new Date();
+	usuario.localizacion = getCurrentPosition();
+}
+usuario.nombre = nombreUsario;
+if(primera){
+
+}
+
+for (let clave in localStorage) {
+	let valor = localStorage[clave];
+	console.log(clave + ": " + valor)
+}
